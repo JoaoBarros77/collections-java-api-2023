916c7be841e63d1bf14b5c81c0314683a8a5036b
computador <joaobarros9042@gmail.com>
2024-05-17 09:24:59 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class tarefa {
+    private String descricao;
+
+    public Tarefa(String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+}
